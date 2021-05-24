a519ebfdf6c428a0a0de3e82e5c469c9916224b1
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2021-05-23 21:50:54 -0400

    Project Day #0

--- CrossTheRoad/src/main/java/com/Entitys/Entity.java
+++ CrossTheRoad/src/main/java/com/Entitys/Entity.java
@@ -1,11 +1,26 @@
 package com.Entitys;
 
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import com.joshandjohnny.JavaApp.Constants;
 
 public abstract class Entity {
     
     private Rectangle hitbox;
     private int[] xycoord = new int[2];
+    private BufferedImage[] sprites;
+    private double xVelocity;
+    private double yVelocity;
+
+    public Entity(int x, int y) {
+
+        this.xycoord[0] = x;
+        this.xycoord[1] = y;
+        this.hitbox = new Rectangle(this.xycoord[0], this.xycoord[1], Constants.gameUnit*2, Constants.gameUnit*2);
+
+    }
+
     
 
 
--- CrossTheRoad/src/main/java/com/joshandjohnny/JavaApp/Constants.java
+++ CrossTheRoad/src/main/java/com/joshandjohnny/JavaApp/Constants.java
@@ -1,7 +1,8 @@
+package com.joshandjohnny.JavaApp;
 public class Constants {
     
     public static final int screenWidth = 400;
     public static final int screenHeight = 800;
     public static final int gameUnit = 25;
-//C:\Users\John\OneDrive\CompSci\ProjectsWithJosh\CrossTheRoad\crosstheroad\src\main\java\com\Entitys\Entity.java
+
 }
